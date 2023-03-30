4f70a5fff5466d73441b69c4bdce4e17854c7c5b
VladislavKrs <vlad.14.vladfacel98@gmail.com>
2023-03-30 17:29:51 +0300

    added fix photo

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function resultCount() {
     document.querySelector(".input_result").innerHTML = `
     <div class="position_text"><h1 class="subtitle d-flex">With a weight of ${initialWight} kilograms =</h1></div>
 
-    <div class="position"><img src="./picture/protein.svg" alt="sugar">${result} gram</p></div>`
+    <div class="position"><img src="./picture/bodybuilding_nutrition_protein_fitness_diet_icon_149055.svg" alt="protein">${result} gram</p></div>`
 }
