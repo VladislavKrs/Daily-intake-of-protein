6824cef7e20e7e93cc3603fab93eccbb232bf669
VladislavKrs <vlad.14.vladfacel98@gmail.com>
2023-04-14 14:06:06 +0300

    Added linter and prattier in daily-intake of protein

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function resultCount() {
-    const initialWight = document.getElementById('weight').value;
-    const result = countProtein(initialWight);
-    document.querySelector(".input_result").innerHTML = `
+const value = document.querySelector('#button');
+value.addEventListener('click', () => {
+  const initialWight = document.getElementById('weight').value;
+  const result = countProtein(initialWight);
+  document.querySelector('.input_result').innerHTML = `
     <div class="position_text"><h1 class="subtitle d-flex">With a weight of ${initialWight} kilograms =</h1></div>
 
-    <div class="position"><img src="./picture/bodybuilding_nutrition_protein_fitness_diet_icon_149055.svg" alt="protein">${result} gram</p></div>`
-}
+    <div class="position"><img src="./picture/bodybuilding_nutrition_protein_fitness_diet_icon_149055.svg" alt="protein">${result} gram</p></div>`;
+});
